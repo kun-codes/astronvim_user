7155a3379d0db1090acee50cb51fb36853acf94c
throwaway69420-69420 <77796630+throwaway69420-69420@users.noreply.github.com>
2023-01-25 23:43:54 +0530

    modified register.lua

--- which-key/register.lua
+++ which-key/register.lua
@@ -6,7 +6,6 @@ return {
                         -- third key is the key to bring up next level and its displayed
                         -- group name in which-key top level menu
                         ["b"] = { name = "Buffer" },
-                        -- ["r"] = { name = "Run" },
                 },
         },
 }
