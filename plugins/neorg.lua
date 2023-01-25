46fb862cbcdb6d15f1cd7ed5850021763d58d0e3
throwaway69420-69420 <77796630+throwaway69420-69420@users.noreply.github.com>
2023-01-25 22:58:52 +0530

    deleted neorg.lua

--- plugins/neorg.lua
+++
@@ -1,8 +0,0 @@
-return {
-  ["core.norg.completion"] = {
-    config = { -- Note that this table is optional and doesn't need to be provided
-      -- Configuration here
-      engine = "nvim-cmp",
-    },
-  },
-}
