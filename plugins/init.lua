a88f655b3452032f65de088a11d2552d32292592
throwaway69420-69420 <77796630+throwaway69420-69420@users.noreply.github.com>
2023-02-22 18:19:42 +0530

    disabled better-escape

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,10 +1,11 @@
 return {
   -- You can disable default plugins as follows:
   -- ["goolord/alpha-nvim"] = { disable = true },
-
+  ["max397574/better-escape.nvim"] = { disable = true },
   --  ╭──────────────────────────────────────────────────────────╮
   --  │                          themes                          │
   --  ╰──────────────────────────────────────────────────────────╯
+
   ["yashguptaz/calvera-dark.nvim"] = {},
   ["folke/tokyonight.nvim"] = {
     config = function()
@@ -31,7 +32,6 @@ return {
     after = { "nvim-cmp" },
     config = function() require("tabout").setup(require "user.plugins.tabout") end,
   },
-
   --  ╭─────────────────────────────────────────────────────────────╮
   --  │ manages projects and also changes the neovim root directory │
   --  ╰─────────────────────────────────────────────────────────────╯
@@ -40,7 +40,6 @@ return {
     config = function() require("project_nvim").setup(require "user.plugins.project_nvim") end,
     -- activated in "user.plugins.telescope"
   },
-
   -- ╭──────────────────────────────────────────────────────────────────────────────╮
   -- │                          runs individual code files                          │
   -- ╰──────────────────────────────────────────────────────────────────────────────╯
@@ -50,36 +49,30 @@ return {
     config = function() require("code_runner").setup(require "user.plugins.code_runner") end,
     cmd = { "RunCode", "RunFile", "RunProject", "RunClose", "CRFiletype", "CRProjects" },
   },
-
   -- stylize folds in neovim
   ["anuvyklack/pretty-fold.nvim"] = {
     -- event = "User isfolded",
     event = "BufRead",
     config = function() require("pretty-fold").setup(require "user.plugins.pretty-fold") end,
   },
-
   ["folke/todo-comments.nvim"] = {
     event = "BufRead",
     config = function() require("todo-comments").setup() end,
   },
-
   -- Virtual text context for functions and loops for neovim treesitter
   ["haringsrob/nvim_context_vt"] = {
     wants = { "nvim-treesitter" },
     config = function() require("nvim_context_vt").setup(require "user.plugins.nvim_context_vt") end,
     event = { "BufEnter" },
   },
-
   ["folke/zen-mode.nvim"] = {
     cmd = { "ZenMode" },
     config = function() require("zen-mode").setup() end,
   },
-
   -- view diagnostics
   ["folke/trouble.nvim"] = {
     cmd = "TroubleToggle",
   },
-
   ["LudoPinelli/comment-box.nvim"] = {
     cmd = {
       "CBcatalog",
@@ -94,7 +87,6 @@ return {
       "Cbcline",
     },
   },
-
   -- TODO: lazyload mkdir.nvim
   ["jghauser/mkdir.nvim"] = {
     -- event = "BufWriteCmd",
