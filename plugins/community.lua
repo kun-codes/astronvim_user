f3734586c99e8af96b343a8c9c9ca48ec9e10555
throwaway69420-69420 <77796630+throwaway69420-69420@users.noreply.github.com>
2023-06-29 19:02:09 +0530

    added some astrocommunity plugins

---
+++ plugins/community.lua
@@ -0,0 +1,6 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+  { import = "astrocommunity.motion.hop-nvim" },
+}
