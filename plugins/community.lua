9e276ce85178d62a0820052fb18156d5bf21396d
throwaway69420-69420 <77796630+throwaway69420-69420@users.noreply.github.com>
2023-08-25 19:47:49 +0530

    added rainbow delimiters via astrocommunity

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
-  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.motion.hop-nvim" },
 }
