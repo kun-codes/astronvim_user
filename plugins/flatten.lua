d6fdbfac5b209f4815b5af48b1408a2f47d53052
throwaway69420-69420 <77796630+throwaway69420-69420@users.noreply.github.com>
2023-08-25 19:51:36 +0530

    added willothy/flatten.nvim

---
+++ plugins/flatten.lua
@@ -0,0 +1,15 @@
+return {
+  "willothy/flatten.nvim",
+  opts = {
+    callbacks = {
+      pre_open = function()
+        -- Close toggleterm when an external open request is received
+        require("toggleterm").toggle(0)
+      end,
+      block_end = function()
+        -- After blocking ends (for a git commit, etc), reopen the terminal
+        require("toggleterm").toggle(0)
+      end,
+    },
+  },
+}
